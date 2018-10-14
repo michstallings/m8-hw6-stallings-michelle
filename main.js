48d316054e12556a6739057ae25d129382bb620b
michstallings <contact@michellestallings.com>
2018-10-14 14:35:49 -0700

    Failed attempts at fixing functionality

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
-var button = document.getElementsByTagName('generator-button');
+var button = document.getElementsByTagName(button);
 
 var yourFortune = document.querySelector("#fortune-wrapper p span");
 
@@ -19,3 +19,4 @@ function shakeBall () {
 };
 
 button.onclick = function() {shakeBall};
+
