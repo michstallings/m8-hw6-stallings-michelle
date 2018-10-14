96f628990c5e66174adaac3c52f6d9bc3e8309ab
michstallings <contact@michellestallings.com>
2018-10-14 11:28:30 -0700

    Attempted fix for functionality

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
-function shakeBall () {
-	magicFortunes.sort (function(a, b){return 0.5 - Math.random()});
-}
-
 var button = document.getElementsByTagName('generator-button');
-
-button.onclick = function() {shakeBall};
 
 var yourFortune = document.querySelector("#fortune-wrapper p span");
 
 var fortuneTextPlaceHolder = document.createTextNode('');
 
-yourFortune.appendChild(fortuneTextPlaceHolder);
-
 var fortuneText = document.createTextNode(magicFortunes[0]);
 
+yourFortune.appendChild(fortuneTextPlaceHolder);
+
 yourFortune.appendChild(fortuneText);
+
+button.onclick = function() {shakeBall};
+
+function shakeBall () {
+	magicFortunes.sort (function(a, b){return 0.5 - Math.random()});
+}
 
 yourFortune.classList.add("fortune-show");
