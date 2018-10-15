092510d7a377a9eee34f439dca597304f7b40628
michstallings <contact@michellestallings.com>
2018-10-14 17:30:08 -0700

    Got the function to begin working correctly

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
+var magicFortunes = [
+'It is certain.',
+'It is decidedly so.', 
+'Without a doubt.', 
+'Yes - definitely.', 
+'You may rely on it.', 
+'As I see it, yes.', 
+'Most likely.', 
+'Outlook good.', 'Yes.',
+'Signs point to yes.', 
+'Reply hazy, try again', 
+'Ask again later.', 
+'Better not tell you now.',
+'Cannot predict now.', 
+'Concentrate and ask again.', 
+'Dont count on it.', 
+'My reply is no.', 
+'My sources say no.', 
+'Outlook not so good.', 
+'Very doubtful.'];
 
-var button = document.getElementsByTagName(button);
+var button = document.getElementsByTagName("button"[0]);
 
 var yourFortune = document.querySelector("#fortune-wrapper p span");
 
 var fortuneTextPlaceHolder = document.createTextNode('');
 
-var fortuneText = document.createTextNode(magicFortunes[0]);
-
-yourFortune.appendChild(fortuneTextPlaceHolder);
-
-yourFortune.appendChild(fortuneText);
-
-yourFortune.classList.add("fortune-show");
 
 function shakeBall () {
-	magicFortunes.sort (function(a, b){return 0.5 - Math.random()});
-};
+	var fortuneText = document.createTextNode(magicFortunes[0]);
 
-button.onclick = function() {shakeBall};
+	yourFortune.appendChild(fortuneTextPlaceHolder);
+
+	yourFortune.appendChild(fortuneText);
+
+	yourFortune.classList.add("fortune-show");
+
+	magicFortunes.sort (function(a, b){return 0.5 - Math.random()});
+
+	
+};
 
